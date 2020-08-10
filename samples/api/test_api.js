7e6468a6978029cb328c6cfc1b9efc328ce7cc05
jose pablo <jose@oceanprotocol.com>
2020-08-10 09:27:41 +0200

    simple APi initialization

--- samples/api/test_api.js
+++ samples/api/test_api.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 const VerifyAPI = require('../../api/api.js')
 const MockWallet = require('../mockWallet')
-const {LotusRPC} = require('@filecoin-shipyard/lotus-client-rpc')
-const {NodejsProvider: Provider} = require('@filecoin-shipyard/lotus-client-provider-nodejs')
-const {testnet} = require('@filecoin-shipyard/lotus-client-schema')
+//const {LotusRPC} = require('@filecoin-shipyard/lotus-client-rpc')
+//const {NodejsProvider: Provider} = require('@filecoin-shipyard/lotus-client-provider-nodejs')
+//const {testnet} = require('@filecoin-shipyard/lotus-client-schema')
 const constants = require("../constants")
 
 async function run () {
@@ -11,6 +11,7 @@ async function run () {
     let endpointUrl = constants.lotus_endpoint
     let tokenPath = constants.token_path 
 
+    /*
     const provider = new Provider(endpointUrl, {
         token: async () => {
             return fs.readFileSync(tokenPath)
@@ -18,12 +19,18 @@ async function run () {
     })
 
     const client = new LotusRPC(provider, { schema: testnet.fullNode })
-  
+  */
     const mnemonic = 'exit mystery juice city argue breeze film learn orange dynamic marine diary antenna road couple surge marine assume loop thought leader liquid rotate believe'
     const path = "m/44'/1'/1/0/"
     const mockWallet = new MockWallet(mnemonic, path)
 
-    const api = new VerifyAPI(client, mockWallet)
+    //const api = new VerifyAPI(client, mockWallet)
+
+    const api = new VerifyAPI(VerifyAPI.standAloneProvider(endpointUrl, {
+        token: async () => {
+            return fs.readFileSync(tokenPath)
+        }     
+    }), mockWallet)
     
     var address= "t1rxk2ynia27jb6cs5r7xcgydm72p5ouu4js6lsbi"
     var datacap = 10000000000000000000000n
