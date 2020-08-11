374b9b0bcd1151eede1a6f173abe1880eea45bde
Sami Mäkelä <mrsmkl@gmail.com>
2020-08-11 16:09:15 +0300

    vscode indent

--- api/api.js
+++ api/api.js
@@ -1,8 +1,5 @@
-const {testnet} = require('@filecoin-shipyard/lotus-client-schema')
+const { testnet } = require('@filecoin-shipyard/lotus-client-schema')
 const hamt = require('../hamt/hamt')
-// const CID = require('cids')
-const fs = require('fs')
-const address = require('@openworklabs/filecoin-address')
 const methods = require('../filecoin/methods')
 const { BrowserProvider: BrowserProvider } = require('@filecoin-shipyard/lotus-client-provider-browser')
 const { NodejsProvider: NodejsProvider } = require('@filecoin-shipyard/lotus-client-provider-nodejs')
@@ -10,7 +7,7 @@ const { LotusRPC } = require('@filecoin-shipyard/lotus-client-rpc')
 
 class VerifyAPI {
 
-    constructor (lotusClient, walletContext) {
+    constructor(lotusClient, walletContext) {
         this.client = lotusClient
         this.walletContext = walletContext
     }
@@ -26,125 +23,121 @@ class VerifyAPI {
     }
 
     async load(a) {
-        try{
+        try {
             let res = await this.client.chainGetNode(a)
             return res.Obj
-        }catch (err) {
+        } catch (err) {
             throw err
-          }
+        }
     }
 
     async listVerifiers() {
 
-        try{
+        try {
             const head = await this.client.chainHead()
             const state = head.Blocks[0].ParentStateRoot['/']
             const verifiers = (await this.client.chainGetNode(`${state}/@Ha:t06/1/1`)).Obj
-        
-            return  await hamt.buildArrayData(verifiers, this.load)
-        }catch (err) {
+
+            return await hamt.buildArrayData(verifiers, this.load)
+        } catch (err) {
             throw err
-          }
-      
-   }
+        }
 
-   async checkVerifier(verifierAddress) {
+    }
 
-    try{
-        // empty array if not verifier is present
-        return this.listVerifiers
-                    .filter( verifier => verifier[0].toString() === verifierAddress)
-    }catch (err) {
-        throw err
-      }
+    async checkVerifier(verifierAddress) {
 
-   }
+        try {
+            // empty array if not verifier is present
+            return this.listVerifiers
+                .filter(verifier => verifier[0].toString() === verifierAddress)
+        } catch (err) {
+            throw err
+        }
 
-   async proposeVerifier(verifierAccount, datacap, indexAccount) {
+    }
 
-    if ( typeof this.walletContext === 'undefined' || !this.walletContext)
-        throw new Error("No wallet context defined in API")
+    async proposeVerifier(verifierAccount, datacap, indexAccount) {
 
-        try{
+        if (typeof this.walletContext === 'undefined' || !this.walletContext)
+            throw new Error("No wallet context defined in API")
+
+        try {
             // Not address but account in the form "t01004", for instance
             let tx = methods.rootkey.propose(methods.verifreg.addVerifier(verifierAccount, datacap))
             let res = await methods.sendTx(this.client, indexAccount, this.walletContext, tx)
             // res has this shape: {/: "bafy2bzaceb32fwcf7uatfxfs367f3tw5yejcresnw4futiz35heb57ybaqxvu"}
             // we return the messageID
             return res['/']
-        }catch (err) {
+        } catch (err) {
             throw err
-          }
+        }
 
     }
 
-     async approveVerifier(verifierAccount, datacap, fromAccount, transactionId, indexAccount) {
+    async approveVerifier(verifierAccount, datacap, fromAccount, transactionId, indexAccount) {
 
-        if ( typeof this.walletContext === 'undefined' || !this.walletContext)
+        if (typeof this.walletContext === 'undefined' || !this.walletContext)
             throw new Error("No wallet context defined in API")
-        
-        try{
+
+        try {
             // Not address but account in the form "t01003", for instance
             let add = methods.verifreg.addVerifier(verifierAccount, datacap)
-            console.log("here",add.params.toString("hex"))
+            console.log("here", add.params.toString("hex"))
 
             //let tx = methods.rootkey.approve(0, {...add, from: "t01001"})
-            let tx = methods.rootkey.approve(transactionId, {...add, from: fromAccount})
+            let tx = methods.rootkey.approve(transactionId, { ...add, from: fromAccount })
             console.log(tx)
-   
+
             let res = await methods.sendTx(this.client, indexAccount, this.walletContext, tx)
             // res has this shape: {/: "bafy2bzaceb32fwcf7uatfxfs367f3tw5yejcresnw4futiz35heb57ybaqxvu"}
             // we return the messageID
             return res['/']
-        }catch (err) {
+        } catch (err) {
             throw err
-          }
+        }
 
-    }   
+    }
 
+    async listVerifiedClients() {
 
-   async listVerifiedClients() {
-
-        try{
+        try {
             const head = await this.client.chainHead()
             const state = head.Blocks[0].ParentStateRoot['/']
-            const verified = (await this.client.chainGetNode(`${state}/@Ha:t06/1/2`)).Obj  
-            
-            return await hamt.buildArrayData(verified, this.load)     
-        }catch (err) {
+            const verified = (await this.client.chainGetNode(`${state}/@Ha:t06/1/2`)).Obj
+
+            return await hamt.buildArrayData(verified, this.load)
+        } catch (err) {
             throw err
-          }
-    } 
-      
+        }
+    }
 
     async checkClient(clientAddress) {
 
         try {
             return this.listVerifiedClients
-                        .filter( client => client[0].toString() === clientAddress)
-        }catch (err) {
+                .filter(client => client[0].toString() === clientAddress)
+        } catch (err) {
             throw err
-        }   
+        }
     }
-
 
     async verifyClient(clientAddress, datacap, indexAccount) {
 
-        
-        if ( typeof this.walletContext === 'undefined' || !this.walletContext)
+
+        if (typeof this.walletContext === 'undefined' || !this.walletContext)
             throw new Error("No wallet context defined in API")
 
-        try{ 
+        try {
             let arg = methods.verifreg.addVerifiedClient(clientAddress, datacap)
             let res = await methods.sendTx(this.client, indexAccount, this.walletContext, arg)
             // res has this shape: {/: "bafy2bzaceb32fwcf7uatfxfs367f3tw5yejcresnw4futiz35heb57ybaqxvu"}
             // we return the messageID
             return res['/']
-        }catch (err) {
+        } catch (err) {
             throw err
-          }
+        }
     }
-
 
 }
 
