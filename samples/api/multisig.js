838f48c90ec24861e7909750f39a901369da9267
jose pablo <jose@oceanprotocol.com>
2020-08-13 08:18:24 +0200

    added getPendingTransactions method from sami branch

---
+++ samples/api/multisig.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+const VerifyAPI = require('../../api/api.js')
+const MockWallet = require('../mockWallet')
+const constants = require("../constants")
+
+async function run () {
+
+    let endpointUrl = constants.lotus_endpoint
+
+    const mnemonic = 'exit mystery juice city argue breeze film learn orange dynamic marine diary antenna road couple surge marine assume loop thought leader liquid rotate believe'
+    const path = "m/44'/1'/1/0/"
+    const mockWallet = new MockWallet(mnemonic, path)
+
+    const api = new VerifyAPI(VerifyAPI.standAloneProvider(endpointUrl), mockWallet)
+
+    console.log(Buffer.from("") instanceof Buffer)
+
+    while (true) {
+
+        let info = await api.pendingRootTransactions()
+        console.log(info)
+
+        await new Promise(resolve => { setTimeout(resolve, 1000) })
+    }
+
+}
+
+run()
