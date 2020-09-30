0fcdbef0ae98bbe77d4b9212e646173b15d5ab72
Sami Mäkelä <mrsmkl@gmail.com>
2020-09-30 12:02:08 +0300

    lint

--- samples/api/new-msig.js
+++ samples/api/new-msig.js
@@ -18,13 +18,13 @@ async function main() {
   const accts = await mockWallet.getAccounts()
   console.log(accts)
   const textile = accts[3]
-  const slate = accts[4]
 
   const m0_addr = await api.newMultisig([textile], 1, 3)
   console.log('M0', m0_addr)
 
   /*
 
+  const slate = accts[4]
   const m1_addr = await api.newMultisig([textile, slate], 2, 3)
   console.log('M1', m1_addr)
 
