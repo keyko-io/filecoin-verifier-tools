44a0ed07288397e1ea8192b04cd67b38a7c580cf
jose pablo <jose@oceanprotocol.com>
2020-10-28 13:42:36 +0100

    notary issue parser

---
+++ utils/notary-issue-parser.js
@@ -0,0 +1,61 @@
+
+function parseIssue(issueContent) {
+   
+    const regexName = /-\s*Name:\s*(.*)/m
+    const regexWebsite = /-\s*Website\s*\/\s*Social\s*Media:\s*(.*)/m
+    const regexAddress = /-\s*On-chain\s*Address\(es\)\s*to\s*be\s*Notarized:\s*(.*)/m
+    const regexRegion = /-\s*Region\s*of\s*Operation:\s*(.*)/m
+    const regexUseCases = /-\s*Use\s*case\(s\)\s*to\s*be\s*supported:\s*(.*)/m
+    const regexDatacapRequested = /-\s*DataCap\s*Requested:\s*(.*)/m
+  
+    const name = matchGroup(regexName, issueContent)
+    const website = matchGroup(regexWebsite, issueContent)
+    const address = matchGroup(regexAddress, issueContent)
+    const datacapRequested = matchGroup(regexDatacapRequested, issueContent)
+    const region = matchGroup(regexRegion, issueContent)
+    const useCases = matchGroup(regexUseCases, issueContent)
+  
+    if (name != null && address != null && datacapRequested != null && website != null && region != null && useCases != null)  {
+      return {
+        correct: true,
+        errorMessage: '',
+        errorDetails: '',
+        name: name,
+        address: address,
+        datacapRequested: datacapRequested,
+        website: website,
+        region: region,
+        useCases: useCases,
+      }
+    }
+  
+    let errorMessage = ''
+    if (name == null) { errorMessage += 'We could not find your **Name** in the information provided\n' }
+    if (address == null) { errorMessage += 'We could not find your **Filecoin address** in the information provided\n' }
+    if (datacapRequested == null) { errorMessage += 'We could not find the **Datacap** requested in the information provided\n' }
+    if (website == null) { errorMessage += 'We could not find any **Web site or social media info** in the information provided\n' }
+    if (region == null) { errorMessage += 'We could not find any **Region** in the information provided\n' }
+    if (useCases == null) { errorMessage += 'We could not find any **Use Case** in the information provided\n' }
+  
+    return {
+      correct: false,
+      errorMessage: errorMessage,
+      errorDetails: `Unable to find required attributes.
+        The name= ${name},
+        address= ${address},
+        datacapRequested= ${datacapRequested},
+        website= ${website},
+        region= ${region},
+        useCases= ${useCases}`,
+    }
+  }
+  
+  function matchGroup(regex, content) {
+    let m
+    if ((m = regex.exec(content)) !== null) {
+      if (m.length >= 1) { return m[1] }
+    }
+  }
+
+  exports.parseIssue = parseIssue
+  
