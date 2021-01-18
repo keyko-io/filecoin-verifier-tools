8f3d0aef75633ae9fc466d62e333f7ebe64dc89d
Sami Mäkelä <mrsmkl@gmail.com>
2021-01-18 12:36:46 +0200

    added method to cancel

--- samples/api/add-rootkey.js
+++ samples/api/add-rootkey.js
@@ -1,5 +1,5 @@
 const VerifyAPI = require('../../api/api.js')
-const methods = require('../../filecoin/methods.js')
+const methods = require('../../filecoin/methods.js').mainnet
 const MockWallet = require('../mockWallet')
 const fs = require('fs')
 const constants = require('../constants')
@@ -7,7 +7,7 @@ const constants = require('../constants')
 const endpointUrl = constants.lotus_endpoint
 const tokenPath = constants.token_path
 
-const mockWallet = new MockWallet(constants.rootkey_mnemonic, constants.rootkey_path)
+const mockWallet = new MockWallet(constants.rootkey_mnemonic, constants.path)
 
 const api = new VerifyAPI(VerifyAPI.standAloneProvider(endpointUrl, {
   token: async () => {
@@ -16,7 +16,7 @@ const api = new VerifyAPI(VerifyAPI.standAloneProvider(endpointUrl, {
 }), mockWallet)
 
 async function main() {
-  const tx = methods.rootkey.addSigner(process.argv[2], false)
+  const tx = methods.rootkey.addSigner(process.argv[2], true)
   console.log(tx)
   const tx2 = methods.rootkey.propose(tx)
   console.log(tx2)
