33c9787539ab63f24736acbb011ab823b7b56c09
Iván <ivan.alberquilla@gmail.com>
2021-01-29 12:26:22 +0100

    exp notation

--- filecoin/methods.js
+++ filecoin/methods.js
@@ -122,9 +122,10 @@ function make(testnet) {
   }
 
   async function iterateGas(client, msg) {
-    let maxfee = 10n ** 10n
+    let maxfee = BigInt(Math.pow(Number(BigInt(10)), Number(BigInt(10))))
     let saved_error
-    while (maxfee < 2n * 10n ** 18n) {
+    let maxFeeComparison =  BigInt(2) *  BigInt(Math.pow(Number(BigInt(10)), Number(BigInt(18))))
+    while (maxfee < maxFeeComparison) {
       try {
         maxfee = maxfee * 2n
         const [res, fee] = await estimateGas(client, maxfee, msg)
