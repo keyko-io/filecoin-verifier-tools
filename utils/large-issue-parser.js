e44fba0c214f59b049187aa1dfec24038655ab0e
Jernej Pregelj <jernejpregelj@jernejs-air.localdomain>
2021-04-19 13:31:20 +0200

    large issue parser

---
+++ utils/large-issue-parser.js
@@ -0,0 +1,161 @@
+
+function parseIssue(issueContent, issueTitle = '') {
+  const regexName = /-\s*Name:\s*(.*)/m
+  const regexWebsite = /-\s*Website\s*\/\s*Social\s*Media:\s*(.*)/m
+  const regexAddress = /-\s*On-chain\s*Address\(es\)\s*to\s*be\s*Notarized:\s*(.*)/m
+  const regexUseCases = /-\s*Use\s*case\(s\)\s*to\s*be\s*supported:\s*(.*)/m
+  const regexDatacapRequested = /-\s*DataCap\s*Requested:\s*(.*)/m
+
+  const regextRemovalTitle = /\s*Notary\s*DataCap\s*Removal:\s*(.*)/m
+
+  const name = matchGroup(regexName, issueContent)
+  const website = matchGroup(regexWebsite, issueContent)
+  const address = matchGroup(regexAddress, issueContent)
+  const datacapRequested = matchGroup(regexDatacapRequested, issueContent)
+  const useCases = matchGroup(regexUseCases, issueContent)
+
+  if (name != null && address != null && datacapRequested != null && website != null && region != null && useCases != null) {
+    return {
+      correct: true,
+      errorMessage: '',
+      errorDetails: '',
+      name: name,
+      address: address,
+      datacapRequested: datacapRequested,
+      website: website,
+      useCases: useCases,
+      datacapRemoval: false,
+    }
+  }
+
+  if (issueTitle !== '') {
+    const removalAddress = matchGroup(regextRemovalTitle, issueTitle)
+    if (removalAddress != null) {
+      return {
+        correct: true,
+        errorMessage: '',
+        errorDetails: '',
+        name: '',
+        address: removalAddress,
+        datacapRequested: '0B',
+        website: '',
+        useCases: '',
+        datacapRemoval: true,
+      }
+    }
+  }
+
+  let errorMessage = ''
+  if (name == null) { errorMessage += 'We could not find your **Name** in the information provided\n' }
+  if (address == null) { errorMessage += 'We could not find your **Filecoin address** in the information provided\n' }
+  if (datacapRequested == null) { errorMessage += 'We could not find the **Datacap** requested in the information provided\n' }
+  if (website == null) { errorMessage += 'We could not find any **Web site or social media info** in the information provided\n' }
+  if (useCases == null) { errorMessage += 'We could not find any **Use Case** in the information provided\n' }
+
+  return {
+    correct: false,
+    errorMessage: errorMessage,
+    errorDetails: `Unable to find required attributes.
+        The name= ${name},
+        address= ${address},
+        datacapRequested= ${datacapRequested},
+        website= ${website},
+        useCases= ${useCases}`,
+  }
+}
+
+function matchGroup(regex, content) {
+  let m
+  if ((m = regex.exec(content)) !== null) {
+    if (m.length >= 2) {
+      return m[1]
+    }
+    return m[0]
+  }
+}
+
+function parseApproveComment(commentContent) {
+  const regexApproved = /##\s*Request\s*Approved/m
+  const regexAddress = /####\s*Address\W*^>\s*(.*)/m
+  const regexDatacap = /####\s*Datacap\s*Allocated\W*^>\s*(.*)/m
+
+  const approved = matchGroup(regexApproved, commentContent)
+
+  if (approved == null) {
+    return {
+      approvedMessage: false,
+    }
+  }
+
+  const address = matchGroup(regexAddress, commentContent)
+  const datacap = matchGroup(regexDatacap, commentContent)
+
+  if (address != null && datacap != null) {
+    return {
+      approvedMessage: true,
+      correct: true,
+      address: address,
+      datacap: datacap,
+    }
+  }
+
+  let errorMessage = ''
+  if (address == null) { errorMessage += 'We could not find the **Filecoin address** in the information provided in the comment\n' }
+  if (datacap == null) { errorMessage += 'We could not find the **Datacap** allocated in the information provided in the comment\n' }
+  return {
+    approvedMessage: true,
+    correct: false,
+    errorMessage: errorMessage,
+    errorDetails: `Unable to find required attributes.
+          The address= ${address},
+          datacapAllocated= ${datacap}`,
+  }
+}
+
+function matchAll(regex, content) {
+  var matches = [...content.matchAll(regex)]
+  if (matches !== null) {
+    // each entry in the array has this form: Array ["#### Address > f1111222333", "", "f1111222333"]
+    return matches.map(elem => elem[2])
+  }
+}
+
+function parseMultipleApproveComment(commentContent) {
+  const regexApproved = /##\s*Request\s*Approved/m
+  const regexAddress = /####\s*Address\s*(.*)\n>\s*(.*)/g
+  const regexDatacap = /####\s*Datacap\s*Allocated\s*(.*)\n>\s*(.*)/g
+
+  const approved = matchGroup(regexApproved, commentContent)
+
+  if (approved == null) {
+    return {
+      approvedMessage: false,
+    }
+  }
+
+  const datacaps = matchAll(regexDatacap, commentContent)
+  const addresses = matchAll(regexAddress, commentContent)
+
+  if (addresses != null && datacaps != null) {
+    return {
+      approvedMessage: true,
+      correct: true,
+      addresses: addresses,
+      datacaps: datacaps,
+    }
+  }
+
+  let errorMessage = ''
+  if (addresses == null) { errorMessage += 'We could not find the **Filecoin address** in the information provided in the comment\n' }
+  if (datacaps == null) { errorMessage += 'We could not find the **Datacap** allocated in the information provided in the comment\n' }
+  return {
+    approvedMessage: true,
+    correct: false,
+    errorMessage: errorMessage,
+    errorDetails: 'Unable to find required attributes.',
+  }
+}
+
+exports.parseIssue = parseIssue
+exports.parseApproveComment = parseApproveComment
+exports.parseMultipleApproveComment = parseMultipleApproveComment
---
+++ utils/large-issue-parser.test.js
@@ -0,0 +1,79 @@
+var fs = require('fs')
+var path = require('path')
+const { parseIssue, parseApproveComment, parseMultipleApproveComment } = require('./notary-issue-parser')
+
+describe('parseIssue()', () => {
+  it('we can parse an issue including the right data', () => {
+    const issueContent = fs.readFileSync(
+      path.resolve(__dirname, '../samples/utils/notary_application.test.md'),
+      { encoding: 'utf8' },
+    )
+    const parsedResult = parseIssue(issueContent)
+
+    expect(parsedResult.correct).toBe(true)
+    expect(parsedResult.name).toBe('Notary A')
+    expect(parsedResult.address).toBe('f1111222333')
+    expect(parsedResult.datacapRequested).toBe('10TB')
+    expect(parsedResult.website).toBe('info.org')
+    expect(parsedResult.region).toBe('[North America]')
+    expect(parsedResult.useCases).toBe('[Developer Tools, Web 3.0]')
+  })
+
+  it('we can not parse an invalid issue', () => {
+    const parsedResult = parseIssue('random string')
+    expect(parsedResult.correct).toBe(false)
+    expect(parsedResult.errorMessage).not.toBe('')
+  })
+})
+
+describe('parseRemovalIssue()', () => {
+  it('we can parse an issue including the right data', () => {
+    const issueContent = 'This is an issue to remove the DataCap associated with f1sdzgaqmitbvgktkklpuaxohg6nuhce5eyvwxhbb.\nThis address was used by the Filecoin Foundation during the Filecoin Beta for allocations. Now that a new allocation has been made to a new address, this should be set to 0.'
+    const issueTitle = 'Notary DataCap Removal: f1sdzgaqmitbvgktkklpuaxohg6nuhce5eyvwxhbb'
+
+    const parsedResult = parseIssue(issueContent, issueTitle)
+
+    expect(parsedResult.correct).toBe(true)
+    expect(parsedResult.datacapRemoval).toBe(true)
+    expect(parsedResult.address).toBe('f1sdzgaqmitbvgktkklpuaxohg6nuhce5eyvwxhbb')
+    expect(parsedResult.datacapRequested).toBe('0B')
+  })
+})
+
+describe('parseApproved()', () => {
+  it('we can parse an approve comment including the right data', () => {
+    const commentContent = fs.readFileSync(
+      path.resolve(__dirname, '../samples/utils/notary_approved_comment.test.md'),
+      { encoding: 'utf8' },
+    )
+    const parsedResult = parseApproveComment(commentContent)
+
+    console.log(parsedResult)
+
+    expect(parsedResult.correct).toBe(true)
+    expect(parsedResult.approvedMessage).toBe(true)
+    expect(parsedResult.address).toBe('f1111222333')
+    expect(parsedResult.datacap).toBe('5TiB')
+  })
+})
+
+describe('parseApprovedMultiple()', () => {
+  it('we can parse an approve comment including the right data', () => {
+    const commentContent = fs.readFileSync(
+      path.resolve(__dirname, '../samples/utils/notary_approved_comment.test.md'),
+      { encoding: 'utf8' },
+    )
+    const parsedResult = parseMultipleApproveComment(commentContent)
+
+    console.log(parsedResult)
+
+    expect(parsedResult.correct).toBe(true)
+    expect(parsedResult.approvedMessage).toBe(true)
+    expect(parsedResult.addresses[0]).toBe('f1111222333')
+    expect(parsedResult.datacaps[0]).toBe('5TiB')
+    expect(parsedResult.addresses[1]).toBe('f33332222111')
+    expect(parsedResult.datacaps[1]).toBe('1TiB')
+    expect(parsedResult.addresses[2]).toBe('f222233334444')
+    expect(parsedResult.datacaps[2]).toBe('10TiB')
+  })
+})
