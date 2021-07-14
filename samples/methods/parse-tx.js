fc8a41681fa7515ae1c2438e389f4074936776ec
Sami Mäkelä <mrsmkl@gmail.com>
2021-07-14 11:50:41 +0300

    lint

--- samples/methods/parse-tx.js
+++ samples/methods/parse-tx.js
@@ -1,11 +1,10 @@
 const methods = require('../../filecoin/methods').mainnet
-const cbor = require('cbor')
 
 function main() {
-    console.log(process.argv[2])
-    let arg = JSON.parse(process.argv[2])
-    // console.log(methods.parse({to: 'f06', params: Buffer.from('gkQAn6lDRwBkAAAAAAA=', 'base64'), method: 2}))
-    console.log(methods.parse({to: arg.To, params: Buffer.from(arg.Params, 'base64'), method: arg.Method}))
+  console.log(process.argv[2])
+  const arg = JSON.parse(process.argv[2])
+  // console.log(methods.parse({to: 'f06', params: Buffer.from('gkQAn6lDRwBkAAAAAAA=', 'base64'), method: 2}))
+  console.log(methods.parse({ to: arg.To, params: Buffer.from(arg.Params, 'base64'), method: arg.Method }))
 }
 
 main()
