9cf6e2ca5a0b82263b3e77e5452bcd90f1d81942
Fabriziogianni7 <fabrizio@keyko.io>
2021-10-11 15:32:41 +0200

    added metrics

---
+++ metrics/metrics.js
@@ -0,0 +1,16 @@
+
+export function callMetricsApi(issueNumber, eventType, params) {
+    const req = {
+        environment: process.env.METRICS_API_ENVIRONMENT || "test",
+        issueNumber,
+        repo: "large-dataset",
+        timeStamp: new Date,
+        eventType,
+        params
+    }
+    // const res = "functioning sofar"
+    console.log(req)
+    // const res =  axios.post(baseURL, req)
+    // console.log()
+    // return res
+}
