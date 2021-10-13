477a861f2105c9484b23be92ecea35ebd4fbfb4b
Fabriziogianni7 <fabrizio@keyko.io>
2021-10-13 12:02:57 +0200

    lint fix

--- metrics/metrics.js
+++ metrics/metrics.js
@@ -1,21 +1,21 @@
-const axios = require("axios")
-const baseURL = "https://api.filplus.d.interplanetary.one/api/storeBotEvent"
+const axios = require('axios')
+const baseURL = 'https://api.filplus.d.interplanetary.one/api/storeBotEvent'
 async function callMetricsApi(issueNumber, eventName, eventMetadata) {
-    const req = {
-        environment: process.env.METRICS_API_ENVIRONMENT || "test",
-        issueNumber,
-        repo: "large-dataset",
-        timeStamp: new Date,
-        eventName,
-        eventMetadata
-    }
-    console.log("req", req)
-    try {
-        const res = await axios.post(baseURL, req)
-        console.log(res)
-    } catch (error) {
-        console.error(error)
-    }
+  const req = {
+    environment: process.env.METRICS_API_ENVIRONMENT || 'test',
+    issueNumber,
+    repo: 'large-dataset',
+    timeStamp: new Date(),
+    eventName,
+    eventMetadata,
+  }
+  console.log('req', req)
+  try {
+    const res = await axios.post(baseURL, req)
+    console.log(res)
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 exports.callMetricsApi = callMetricsApi
