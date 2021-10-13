4f04bdbe4d9a201d0ecf496aee2310d2cbc7096a
Fabriziogianni7 <fabrizio@keyko.io>
2021-10-13 15:17:14 +0200

    fix on metrics environment

--- metrics/metrics.js
+++ metrics/metrics.js
@@ -1,22 +1,22 @@
 const axios = require('axios')
 const baseURL = 'https://api.filplus.d.interplanetary.one/api/storeBotEvent'
-const environment = process.env.REACT_APP_METRICS_API_ENVIRONMENT ? "prod" : "test"
-  async function callMetricsApi(issueNumber, eventName, eventMetadata) {
-    const req = {
-      environment,
-      issueNumber,
-      repo: 'large-dataset',
-      timeStamp: new Date(),
-      eventName,
-      eventMetadata,
-    }
-    // console.log('req', req)
-    try {
-      const res = await axios.post(baseURL, req)
-      // console.log(res)
-    } catch (error) {
-      console.error(error)
-    }
+const botEnvironment = process.env.METRICS_API_ENVIRONMENT || 'test'
+async function callMetricsApi(issueNumber, eventName, eventMetadata, environment) {
+  const req = {
+    environment: environment || botEnvironment,
+    issueNumber,
+    repo: 'large-dataset',
+    timeStamp: new Date(),
+    eventName,
+    eventMetadata,
   }
+  //  console.log('req', req)
+  try {
+    await axios.post(baseURL, req)
+    // console.log(res)
+  } catch (error) {
+    console.error(error)
+  }
+}
 
 exports.callMetricsApi = callMetricsApi
