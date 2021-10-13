34bbfb1ca549bb57884f095d7f34efe3d1c0ea66
Fabriziogianni7 <fabrizio@keyko.io>
2021-10-13 12:01:33 +0200

    metrics request

--- metrics/metrics.js
+++ metrics/metrics.js
@@ -1,18 +1,21 @@
-
-function callMetricsApi(issueNumber, eventType, params) {
+const axios = require("axios")
+const baseURL = "https://api.filplus.d.interplanetary.one/api/storeBotEvent"
+async function callMetricsApi(issueNumber, eventName, eventMetadata) {
     const req = {
         environment: process.env.METRICS_API_ENVIRONMENT || "test",
         issueNumber,
         repo: "large-dataset",
         timeStamp: new Date,
-        eventType,
-        params
+        eventName,
+        eventMetadata
     }
-    // const res = "functioning sofar"
-    console.log(req)
-    // const res =  axios.post(baseURL, req)
-    // console.log()
-    // return res
+    console.log("req", req)
+    try {
+        const res = await axios.post(baseURL, req)
+        console.log(res)
+    } catch (error) {
+        console.error(error)
+    }
 }
 
 exports.callMetricsApi = callMetricsApi
