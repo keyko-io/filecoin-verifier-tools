8fb5d514993086b7e37e80b9887937dbbdcf6809
Fabriziogianni7 <fabrizio@keyko.io>
2021-10-27 18:36:08 +0200

    eslint fix

--- metrics/metrics.js
+++ metrics/metrics.js
@@ -13,7 +13,7 @@ async function callMetricsApi(issueNumber, eventType, params, environment) {
   //  console.log('req', req)
   try {
     const res = await axios.post(baseURL, req)
-    console.log("metrics data res", res.data)
+    console.log('metrics data res', res.data)
   } catch (error) {
     console.error(error)
   }
