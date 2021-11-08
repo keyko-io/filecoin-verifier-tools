6d4b81b05275cd9af13976ebcce9159934027664
Fabriziogianni7 <fabrizio@keyko.io>
2021-11-08 12:19:31 +0100

    created common util file for parsers

---
+++ utils/common-utils.js
@@ -0,0 +1,43 @@
+
+function validateIssueDataCap(datacap, weeklyDatacap) {
+    const validateDatacapRegex = /[0-9]{1,3}[PiBT]{3,4}/g
+    const resultCorrectDc = datacap?.match(validateDatacapRegex)
+    const resultCorrectWeeklyDc = weeklyDatacap?.match(validateDatacapRegex)
+
+    return {
+        resultCorrectDc,
+        resultCorrectWeeklyDc
+    }
+
+}
+//used in large-issue-parser and notary-issue-parser
+function matchGroupLargeNotary(regex, content) {
+    let m
+    if ((m = regex.exec(content)) !== null) {
+        if (m.length >= 2) {
+            return m[1].trim()
+        }
+        return m[0].trim()
+    }
+}
+//used is issue-parser.js
+function matchGroup(regex, content) {
+    let m
+    if ((m = regex.exec(content)) !== null) {
+        if (m.length >= 1) { return m[1].trim() }
+    }
+}
+
+
+function matchAll(regex, content) {
+    var matches = [...content.matchAll(regex)]
+    if (matches !== null) {
+        // each entry in the array has this form: Array ["#### Address > f1111222333", "", "f1111222333"]
+        return matches.map(elem => elem[2])
+    }
+}
+
+exports.matchGroupLargeNotary = matchGroupLargeNotary
+exports.matchGroup = matchGroup
+exports.matchAll = matchAll
+exports.validateIssueDataCap = validateIssueDataCap
