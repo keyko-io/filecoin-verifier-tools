518e48afd996ab4a48083ae0be4c5e1b64b320f3
Fabriziogianni7 <fabrizio@keyko.io>
2022-04-14 10:42:00 +0200

    lint

--- api/api.js
+++ api/api.js
@@ -293,49 +293,46 @@ class VerifyAPI {
     return returnList
   }
 
-  async  signAndPushCustomTransaction(from, to, params){
+  async signAndPushCustomTransaction(from, to, params) {
     try {
       const nonce = await this.client.mpoolGetNonce(from)
-      
-      let msg = {
+
+      const msg = {
         // version: 42,
         to,
         from,
         nonce,
-        value: '0', 
+        value: '0',
         gaslimit: 208863,
-        gasfeecap: "100000000",
-        gaspremium: "0",
+        gasfeecap: '100000000',
+        gaspremium: '0',
         method: 1,
-        params
+        params,
       }
-      const walletSignMessage =  await this.client.walletSignMessage(from, msg)
+      const walletSignMessage = await this.client.walletSignMessage(from, msg)
       const pushedMsgId = await this.client.mpoolPush(walletSignMessage)
-      return {success:true, walletSignMessage, pushedMsgId}
-      
+      return { success: true, walletSignMessage, pushedMsgId }
     } catch (error) {
-      return {success:false, error}
+      return { success: false, error }
     }
   }
 
-  async  listMessagesFromToAddress(From, To, heightPerc = 0.5){
+  async listMessagesFromToAddress(From, To, heightPerc = 0.5) {
     try {
-      const head = await api.client.chainHead()
-      const messages = await api.client.stateListMessages(
+      const head = await this.client.chainHead()
+      const messages = await this.client.stateListMessages(
         {
           To,
-          From
-        }, 
+          From,
+        },
         head.Cids,
-        Math.round(head.Height-head.Height*heightPerc) 
+        Math.round(head.Height - head.Height * heightPerc),
       )
-      return messages ? {success:true, messages} :  {success:false}
+      return messages ? { success: true, messages } : { success: false }
     } catch (error) {
-      return {success:false, error} 
+      return { success: false, error }
     }
-
   }
-
 }
 
 module.exports = VerifyAPI
--- samples/api/wallet-sign-and-verify.js
+++ samples/api/wallet-sign-and-verify.js
@@ -1,12 +1,6 @@
 const VerifyAPI = require('../../api/api.js')
 const MockWallet = require('../mockWallet')
-const methods = require('../../filecoin/methods').testnet
-const signer = require('@zondax/filecoin-signing-tools/js')
-const fs = require('fs')
 const constants = require('../constants')
-
-const endpointUrl = constants.lotus_endpoint
-const tokenPath = constants.token_path
 
 const mockWallet = new MockWallet(constants.verifier_mnemonic, constants.path)
 
@@ -21,113 +15,98 @@ const api = new VerifyAPI( // eslint-disable-line
     , {
       token: async () => {
         return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBbGxvdyI6WyJyZWFkIiwid3JpdGUiLCJzaWduIiwiYWRtaW4iXX0.wpOIystriKCXuvbxQnMnYP8tNxgi3Uwn3yeBpeiZJtw'
-      }
+      },
     })
   , mockWallet
   // , { sign: sign, getAccounts: getAccounts }
-  , process.env.NETWORK_TYPE !== "Mainnet" // if node != Mainnet => testnet = true
+  , process.env.NETWORK_TYPE !== 'Mainnet', // if node != Mainnet => testnet = true
 )
 
 async function walletSignMessageApi() {
   try {
-
     const walletList = await api.client.walletList()
     const sender = walletList[1]
-    const nonce =await  api.client.mpoolGetNonce(sender)
+    const nonce = await api.client.mpoolGetNonce(sender)
     console.log(nonce)
-    const to = "t1rbfyvybljzd5xcouqjx22juucdj3xbwtro2crwq"
+    const to = 't1rbfyvybljzd5xcouqjx22juucdj3xbwtro2crwq'
     const from = sender
     const params = 'miao'
 
-  let msg = {
+    const msg = {
     // version: 42,
-    to,
-    from,
-    nonce,
-    value: '0', 
-    gaslimit: 208863,
-    gasfeecap: "100000000",
-    gaspremium: "0",
-    method: 1,
-    params
-  }
- 
-  const walletSignMessage =  await api.client.walletSignMessage(
-    sender,
-    msg
-  )
-  const pushedMsg =  await api.client.mpoolPush(walletSignMessage)
-    console.log("walletSignMessage",walletSignMessage)
-    console.log("Pushed msg:", pushedMsg)
-    
+      to,
+      from,
+      nonce,
+      value: '0',
+      gaslimit: 208863,
+      gasfeecap: '100000000',
+      gaspremium: '0',
+      method: 1,
+      params,
+    }
+
+    const walletSignMessage = await api.client.walletSignMessage(
+      sender,
+      msg,
+    )
+    const pushedMsg = await api.client.mpoolPush(walletSignMessage)
+    console.log('walletSignMessage', walletSignMessage)
+    console.log('Pushed msg:', pushedMsg)
   } catch (error) {
     console.log(error)
   }
 }
-
 
 async function walletVerifyMessage() {
   try {
-    let isValid = false
     const msg = await api.client.chainGetMessage({
-      '/' : 'bafy2bzaceasjil3ydmtw2qgiizgn5wat3zu4vrt3hazkgdw2dmw3qgiccaqvy'
+      '/': 'bafy2bzaceasjil3ydmtw2qgiizgn5wat3zu4vrt3hazkgdw2dmw3qgiccaqvy',
     })
     // console.log('ChainGetMessage:',msg)
-    console.log("parsedMSG", msg)
-    console.log(msg.Params == 'miao' ?  true : false)
-    return msg.Params == 'miao' ?  true : false
-
-
-    
+    console.log('parsedMSG', msg)
+    console.log(msg.Params === 'miao')
+    return msg.Params === 'miao'
   } catch (error) {
     console.log(error)
   }
 }
 
-
-async function findMessage () {
+async function findMessage() {
   console.log(await api.client.chainGetMessage(
-      
-        {"/":"bafy2bzacec64qbngxkapkavd4g6h33maubxasujzj2ms5vss4clteobeslhmg"}
-      
-      
-    ))
 
+    { '/': 'bafy2bzacec64qbngxkapkavd4g6h33maubxasujzj2ms5vss4clteobeslhmg' },
+
+  ))
 }
 
+async function lookForMessages() {
+  try {
+    const head = await api.client.chainHead()
+    // console.log("head", head)
+    // console.log("Block", head.Blocks[0])
+    // const tset = await api.client.chainGetTipSetByHeight(head.Height, head.Cids)
+    // console.log("tset", tset)
 
+    // console.log("messages", head.Blocks.map(item=> item.Messages))
 
-async function lookForMessages () {
-try {
-  
-  const head = await api.client.chainHead()
-  // console.log("head", head)
-  // console.log("Block", head.Blocks[0])
-  // const tset = await api.client.chainGetTipSetByHeight(head.Height, head.Cids)
-  // console.log("tset", tset)
-
-  // console.log("messages", head.Blocks.map(item=> item.Messages))
-  const To = "t1rbfyvybljzd5xcouqjx22juucdj3xbwtro2crwq"
-
-  const messages = await api.client.stateListMessages(
-    {
-      To: "t1rbfyvybljzd5xcouqjx22juucdj3xbwtro2crwq",
-      From: 't13anukxkgvngz46fsjxta3kfzedgnkylfpoff36q'
-    }, 
-    // genesis.Cids,
-    head.Cids,
-    Math.round(head.Height-head.Height*0.5) //0
+    const messages = await api.client.stateListMessages(
+      {
+        To: 't1rbfyvybljzd5xcouqjx22juucdj3xbwtro2crwq',
+        From: 't13anukxkgvngz46fsjxta3kfzedgnkylfpoff36q',
+      },
+      // genesis.Cids,
+      head.Cids,
+      Math.round(head.Height - head.Height * 0.5), // 0
     // 0 //0
-  )
+    )
 
-  console.log('retval', messages)
-} catch (error) {
-  console.log(error)
+    console.log('retval', messages)
+  } catch (error) {
+    console.log(error)
+  }
 }
-}
 
-
-// findMessage()
+findMessage()
 lookForMessages()
-// walletVerifyMessage()
-// walletSignMessageApi()
+walletVerifyMessage()
+walletSignMessageApi()
