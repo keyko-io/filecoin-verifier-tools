7d8606d6298e06aa827807217fb4881d28384abc
hcsoylu <hcsoylu.dev@gmail.com>
2022-11-25 18:28:50 +0300

    added parser test function

--- new_template.js
+++ new_template.js
@@ -1,35 +1,42 @@
-const ownerName = /[\n\r][ \t]*###\s*Data\s*Owner\s*Name[\n\t]*([^\n\r]*)/;
-const webSite = /[\n\r][ \t]*###\s*Website[\n\t]*([^\n\r]*)/;
+const ownerName = /[\n\r]*###\s*Data\s*Owner\s*Name[\n\t]*([^\n\r]*)/
+const web = /[\n\r]*###\s*Website[\n\t]*([^\n\r]*)/
 const ownerCountry =
-  /[\n\r][ \t]*###\s*Data\s*Owner\s*Country[\n\t]*([^\n\r]*)/;
-const total =
-  /[\n\r][ \t]*###\s*Total\s*amount\s*of\s*DataCap\s*being\s*requested[\n\t]*([^\n\r]*)/;
-const weekly =
-  /[\n\r][ \t]*###\s*Weekly\s*allocation\s*of\s*DataCap\s*requested[\n\t]*([^\n\r]*)/;
+  /[\n\r]*###\s*Data\s*Owner\s*Country[\n\t]*([^\n\r]*)/
+const totalData =
+  /[\n\r]*###\s*Total\s*amount\s*of\s*DataCap\s*being\s*requested[\n\t]*([^\n\r]*)/
+const weeklyData =
+  /[\n\r]*###\s*Weekly\s*allocation\s*of\s*DataCap\s*requested[\n\t]*([^\n\r]*)/
 const onchain =
-  /[\n\r][ \t]*###\s*On-chain\s*address\s*for\s*first\s*allocation[\n\t]*([^\n\r]*)/;
+  /[\n\r]*###\s*On-chain\s*address\s*for\s*first\s*allocation[\n\t]*([^\n\r]*)/
 
 function matchGroupLargeNotary(regex, content) {
-  let m;
-  //console.log(regex.exec(content))
+  let m
   if ((m = regex.exec(content)) !== null) {
     if (m.length >= 2) {
-      return m[1].trim();
+      return m[1].trim()
     }
-    return m[0].trim();
+    return m[0].trim()
   }
 }
 
-const issueContent = `
-### Website
+function test_new_template(issueContent) {
+  const name = matchGroupLargeNotary(ownerName, issueContent)
+  const website = matchGroupLargeNotary(web, issueContent)
+  const country = matchGroupLargeNotary(ownerCountry, issueContent)
+  const total = matchGroupLargeNotary(totalData, issueContent)
+  const weekly = matchGroupLargeNotary(weeklyData, issueContent)
+  const address = matchGroupLargeNotary(onchain, issueContent)
 
-www.test.com
-`;
+  const x = {
+    name,
+    website,
+    country,
+    total,
+    weekly,
+    address,
+  }
 
-//const name = matchGroupLargeNotary(ownerName, issueContent)
-//const country = matchGroupLargeNotary(ownerCountry, issueContent)
-const websiteInfo = matchGroupLargeNotary(webSite, issueContent);
+  return x
+}
 
-//console.log(name);
-//console.log(country);
-console.log(websiteInfo);
+exports.test_new_template = test_new_template
