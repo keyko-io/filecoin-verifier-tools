a225fbc01054357b4fa9d09b8703ebde49212560
hcsoylu <hcsoylu.dev@gmail.com>
2022-12-09 21:47:54 +0300

    added new parser logic

--- utils/helpers/old-ldn-parser.js
+++ utils/helpers/old-ldn-parser.js
@@ -1,12 +1,12 @@
 const data = {
-  name: 'Name:',
-  region: 'Region:',
-  website: 'Media:',
-  datacapRequested: 'PiB:',
-  dataCapWeeklyAllocation: '100TiB:',
-  address: 'allocation:',
-  isCustomNotary: 'Type:',
-  identifier: 'Identifier:',
+  name: /Name: (.*)/,
+  region: /Region: (.*)/,
+  website: /Media: (.*)/,
+  datacapRequested: /PiB\): (.*)/,
+  dataCapWeeklyAllocation: /100TiB\): (.*)/,
+  address: /allocation: (.*)/,
+  isCustomNotary: /Type: (.*)/,
+  identifier: /Identifier: (.*)/,
 }
 
 const parsedData = {
@@ -15,37 +15,34 @@ const parsedData = {
   errorDetails: '',
 }
 
-const regexForAdress = /^(f1|f3)/
+// const regexForAdress = /^(f1|f3)/
 
 const text = `
-      - Organization Name: fun
+      - Organization Name: 
       - Website / Social Media: www.fun.com
       - Region: turkey
-      - Total amount of DataCap being requested (between 500 TiB and 5 PiB: 5PiB
-      - Weekly allocation of DataCap requested (usually between 1-100TiB: 100TiB
+      - Total amount of DataCap being requested (between 500 TiB and 5 PiB): 5PiB
+      - Weekly allocation of DataCap requested (usually between 1-100TiB): 100TiB
       - On-chain address for first allocation: f1rbfyvybljzd5xcouqjx22juucdj3xbwtro2crwq
-      - Type: aynen
-      - Identifier: pasam
+      - Type: 
+      - Identifier: 
    `
 
 for (const [key, value] of Object.entries(data)) {
-  const rg = new RegExp(`(?<=${value})(.*)`)
+  const regex = value
 
-  const regexIsNull = !text.match(rg) || !text.match(rg)[0].trim()
+  const match = text.match(regex)
 
-  if ((key === 'identifier' || key === 'isCustomNotary') && regexIsNull) continue
-
-  if (regexIsNull) {
+  if (match[1].trim() === '') {
+    if (key === 'isCustomNotary' || key === 'identifier') continue
     parsedData.correct = false
-    parsedData.errorMessage += `We could not find **${value}** field in the information provided\n`
-    if (parsedData.errorDetails !== '') parsedData.errorDetails = 'Unable to find required attributes.'
+    parsedData.errorMessage += `We could not find **${key}** field in the information provided\n`
+    parsedData[key] = ''
     continue
   }
 
-  parsedData[key] = rg.exec(text)[0].trim()
-
-  if (key === 'address') {
-    parsedData['isAddressFormatted'] = regexForAdress.test(parsedData[key]) //eslint-disable-line
+  if (match && match.length > 1) {
+    parsedData[key] = match[1].trim()
   }
 }
 
