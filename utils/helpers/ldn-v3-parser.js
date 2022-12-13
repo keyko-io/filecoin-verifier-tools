8511729010d6b88e11cf47b604896f5bece7a76a
hcsoylu <hcsoylu.dev@gmail.com>
2022-12-13 17:44:56 +0300

    added new v3 parser function

---
+++ utils/helpers/ldn-v3-parser.js
@@ -0,0 +1,44 @@
+/* eslint-disable indent */
+function v3TriggerParser(commentBody) {
+    const data = {
+        regexTrigger: /##\s*datacap\s*request\s*trigger/mi,
+        totalDatacap: /Total DataCap requested[\S\s]*?>(.*?)\n/,
+        weeklyDatacap: /Expected weekly DataCap usage rate[\S\s]*?>(.*?)\n/,
+    }
+
+    const parsedData = {
+        correct: true,
+        errorMessage: '',
+    }
+
+    for (const [key, value] of Object.entries(data)) {
+        const regex = value
+
+        if (key === 'regexTrigger') {
+            const triggerMessageMatch = regex.test(commentBody)
+            if (!triggerMessageMatch) {
+                return {
+                    triggerMessage: false,
+                }
+            }
+        } else {
+            const match = commentBody.match(regex)
+
+            if (match[1]?.trim() === '') {
+                parsedData.correct = false
+                parsedData.errorMessage += `We could not find **${key}** field in the information provided\n`
+            } else if (!match[1]?.trim().endsWith('iB')) {
+                parsedData.correct = false
+                parsedData.errorMessage += `DataCap info should ends with TiB or PiB : **${key}**\n`
+            }
+
+            if (match) {
+                parsedData[key] = match[1].trim()
+            }
+        }
+    }
+
+    return parsedData
+}
+
+exports.v3TriggerParser = v3TriggerParser
