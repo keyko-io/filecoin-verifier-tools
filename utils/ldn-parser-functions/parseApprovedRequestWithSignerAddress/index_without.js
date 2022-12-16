a5915e9d863ea9d0aaf83bd67a6107fd5e734eee
hcsoylu <hcsoylu.dev@gmail.com>
2022-12-16 18:20:41 +0300

    move index another file

---
+++ utils/ldn-parser-functions/parseApprovedRequestWithSignerAddress/index_without.js
@@ -0,0 +1,42 @@
+/* eslint-disable indent */
+function parseApprovedRequestWithSignerAddress(commentBody) {
+    const data = {
+        method: /##\s*Request\s*((Approved)|(Proposed))/m,
+        address: /#### Address\s*>\s*(.*)/,
+        datacap: /#### Datacap Allocated\s*>\s*(.*)/,
+        signerAddress: /#### Signer Address\s*>\s*(.*)/,
+        message: /#### Message sent to Filecoin Network\s*>\s*(.*)/,
+        uuid: /#### Id\s*>\s*(.*)/,
+    }
+
+    let parsedData = {
+        correct: true,
+        approvedMessage: true,
+        errorMessage: '',
+    }
+
+    for (const [key, value] of Object.entries(data)) {
+        const regex = value
+
+        const match = commentBody.match(regex)
+
+        if (!match) {
+            if (key === 'method') {
+                parsedData = {
+                    approvedMessage: false,
+                }
+                break
+            }
+            parsedData.correct = false
+            parsedData.errorMessage += `We could not find **${key}** field in the information provided\n`
+        }
+
+        if (match) {
+            parsedData[key] = match[1].trim()
+        }
+    }
+
+    return parsedData
+}
+
+exports.parseApprovedRequestWithSignerAddress = parseApprovedRequestWithSignerAddress
