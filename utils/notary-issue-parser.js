ebec2c4f3816e08e258cde6a858a326375a6b680
hcsoylu <hcsoylu.dev@gmail.com>
2022-12-28 17:51:27 +0300

    modul export

--- utils/notary-issue-parser.js
+++ utils/notary-issue-parser.js
@@ -1,11 +1,11 @@
 const { parseNotaryAddress } = require('./notary-parser-functions/parseNotaryAddress')
 const { parseNotaryLedgerVerifiedComment } = require('./notary-parser-functions/parseNotaryLedgerVerifiedComment')
 const { parseApproveComment } = require('./notary-parser-functions/parseApproveComment')
-const { parseMultipleApproveComment } = require('./notary-parser-functions/parseMultipleApproveComment')
 const { parseIssue } = require('./notary-parser-functions/parseIssue')
 
-exports.parseIssue = parseIssue
-exports.parseApproveComment = parseApproveComment
-exports.parseMultipleApproveComment = parseMultipleApproveComment
-exports.parseNotaryLedgerVerifiedComment = parseNotaryLedgerVerifiedComment
-exports.parseNotaryAddress = parseNotaryAddress
+module.exports = {
+  parseIssue,
+  parseApproveComment,
+  parseNotaryLedgerVerifiedComment,
+  parseNotaryAddress,
+}
