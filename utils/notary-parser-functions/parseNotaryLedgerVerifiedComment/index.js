f201ca7dda48b9b3cbda8910fb6d12386dcba3b9
hcsoylu <hcsoylu.dev@gmail.com>
2022-12-29 16:17:28 +0300

    delete comments

--- utils/notary-parser-functions/parseNotaryLedgerVerifiedComment/index.js
+++ utils/notary-parser-functions/parseNotaryLedgerVerifiedComment/index.js
@@ -2,22 +2,18 @@ const { matchGroupLargeNotary } = require('../../common-utils')
 
 function parseNotaryLedgerVerifiedComment(commentContent) {
   const regexVerified = /##\s*Notary\s*Ledger\s*Verified/m
-  // const regexMessageCid = />\s*Message\s*CID:\s*(.*)/m
 
   const verified = matchGroupLargeNotary(regexVerified, commentContent)
-
-  // const messageCid = matchGroupLargeNotary(regexMessageCid, commentContent)
 
   if (verified) {
     return {
       correct: true,
-      // messageCid: messageCid,
     }
   }
 
   let errorMessage = ''
   if (!verified) { errorMessage += 'The issue is not verified\n' }
-  // if (!messageCid) { errorMessage += 'Message CID not found in the comment\n' }
+
   return {
     correct: false,
     errorMessage: errorMessage,
