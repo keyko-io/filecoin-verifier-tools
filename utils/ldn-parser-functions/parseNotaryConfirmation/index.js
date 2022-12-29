da2a8bd76d9cc5dc7eb14dfdb3e411358a4c6cf2
hcsoylu <hcsoylu.dev@gmail.com>
2022-12-29 16:18:50 +0300

    fixed bugs parseNotaryConfirmation

--- utils/ldn-parser-functions/parseNotaryConfirmation/index.js
+++ utils/ldn-parser-functions/parseNotaryConfirmation/index.js
@@ -1,22 +1,21 @@
-/* eslint-disable indent */
-const { matchGroupLargeNotary } = require('../../common-utils')
-
 function parseNotaryConfirmation(commentContent, title) {
-    const regexConfirmation = /##\s*The\s*request\s*has\s*been\s*signed\s*by\s*a\s*new\s*Root\s*Key\s*Holder/m
-    const regexTitleNumber = /Large\sdataset\smultisig\srequest\s#\s*([0-9]*)/m
+  const result = title.match(/#(\d+)/)
 
-    const confirmation = matchGroupLargeNotary(regexConfirmation, commentContent)
-    const number = Number([...title.match(regexTitleNumber)][1])
+  const confirmationText = '## The request has been signed by a new Root Key Holder'
 
-    if (confirmation == null) {
-        return {
-            confirmationMessage: false,
-        }
-    } else {
-        return {
-            confirmationMessage: true,
-            number: number,
-        }
+  const confirmationMessage = commentContent.includes(confirmationText)
+
+  if (!result) {
+    return {
+      errorMessage: 'no title found',
+      confirmationMessage,
     }
+  }
+
+  return {
+    confirmationMessage,
+    number: Number(result[1]),
+  }
 }
+
 exports.parseNotaryConfirmation = parseNotaryConfirmation
