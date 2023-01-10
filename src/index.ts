de17fdc90263b1bcc57fecb593e975b4a8deb3f6
Fabriziogianni7 <fabrizio@keyko.io>
2023-01-10 14:36:52 +0100

    small updates

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 export * from "./api/api";
 export * from "./filecoin/methods";
 export * from "./filecoin/methods-mapping";
-export * from "./metrics/metrics";
+export * as metrics from "./metrics/metrics";
 export * from "./spreadsheet/spreadsheetFiller";
+export * as simpleClientParser from "./utils/simple-client-functions/issue-parser";
 export * as ldnParser from "./utils/large-issue-parser";
 export * as notaryParser from "./utils/notary-issue-parser";
+export * as commonUtils from "./utils/common-utils";
 
 // TODO: convert metrics to use ES
